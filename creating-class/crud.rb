1dbfd15df28957cad42d2650e278cb1e97fce396
audrey45 <arceniokas@gmail.com>
2019-02-05 16:12:32 +0800

    md5 changes

---
+++ creating-class/crud.rb
@@ -0,0 +1,28 @@
+require 'bcrypt'
+
+module Crud
+users = [
+    { username: "mashrur", password: "password1" },
+    { username: "jack", password: "password2" },
+    { username: "arya", password: "password3" },
+    { username: "jonshow", password: "password4" },
+    { username: "heisenberg", password: "password5" }
+  ]
+
+
+  def create_hash_digest(password)
+    BCrypt::Password.create(password)
+  end
+
+  def verify_hash_digest(password)
+    BCrypt::Password.new(password)
+  end
+
+  def create_secure_users(user_list)
+    user_list.each do |user|
+        user[:password] = create_hash_digest(user[:password])
+    end
+   
+    user_list
+  end
+end
