41cf604752c86669154515865966c66e2d0fa6fc
audrey45 <arceniokas@gmail.com>
2019-02-05 16:33:30 +0800

    calling module in other file

--- creating-class/crud.rb
+++ creating-class/crud.rb
@@ -1,28 +1,19 @@
 require 'bcrypt'
 
 module Crud
-users = [
-    { username: "mashrur", password: "password1" },
-    { username: "jack", password: "password2" },
-    { username: "arya", password: "password3" },
-    { username: "jonshow", password: "password4" },
-    { username: "heisenberg", password: "password5" }
-  ]
 
-
-  def create_hash_digest(password)
+  def self.create_hash_digest(password)
     BCrypt::Password.create(password)
   end
 
-  def verify_hash_digest(password)
+  def self.verify_hash_digest(password)
     BCrypt::Password.new(password)
   end
 
-  def create_secure_users(user_list)
+  def self.create_secure_users(user_list)
     user_list.each do |user|
         user[:password] = create_hash_digest(user[:password])
     end
-   
     user_list
   end
 end
